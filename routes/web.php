6d026deb8c84003466b088f67e366f6e9b9ddce7
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-01-09 09:58:02 -0500

    [add] PageController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,16 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', [PageController::class, 'index'])->name('home');
 
 
-Route::get('/about', function() {
-    return view('about');
-})->name('about');
+Route::get('/about', [PageController::class, 'about'])->name('about');
 
 
-Route::get('/movies', function() {
-    return view('movies');
-})->name('movies');
+Route::get('/movies', [PageController::class, 'movies'])->name('movies');
