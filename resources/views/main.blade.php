8cfeb5d6dc2ec3dad83ed4d3d9f3c8979545fd62
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-01-09 08:57:24 -0500

    First Commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel Model Controller</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+
+    <h1>Ciao</h1>
+
+</body>
+
+</html>
